26e37fd7903dda2bd6f15ab884795d5dff70937d
Summer <su943515688@gmail.com>
2026-01-28 19:57:00 +0800

    fix: Update baseUrl to correct GitHub Pages domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "summerkk.github.io/my-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
